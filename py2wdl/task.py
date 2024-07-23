133de587d69796e5d1a1e5156b71d7a6d6b3c9eb
joonyoung1 <joonyoung1023@gmail.com>
2024-07-23 17:02:22 +0900

    Refactor Task to manage dependencies within workflow #1

--- py2wdl/task.py
+++ py2wdl/task.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import inspect
 from textwrap import dedent
 from typing import Callable, Any, Dict
@@ -15,26 +16,18 @@ class Task:
         self.input_type = input_type
         self.output_type = output_type
         self.meta = meta
-        self.args = []
-        self.kwargs = {}
 
-    def set_args(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
-
-    def execute(self) -> Any:
-        return self.func(*self.args, **self.kwargs)
+    def execute(self, *args: Any, **kwargs: Any) -> Any:
+        return self.func(*args, **kwargs)
 
     def __str__(self) -> str:
-        args_str = ", ".join(repr(arg) for arg in self.args)
-        kwargs_str = ", ".join(f"{k}={v!r}" for k, v in self.kwargs.items())
+        if self.meta:
+            meta_str = ", ".join(f"{k}={v!r}" for k, v in self.meta.items())
         func_source = dedent(inspect.getsource(self.func)).strip()
 
         return (
-            f"Arguments: ({args_str})\n"
-            f"Keyword Arguments: ({kwargs_str})\n"
-            f"Function Source:\n{func_source}"
-        )
+            f"Metadata: ({meta_str})\n" if self.meta else ""
+        ) + f"Function Source:\n{func_source}"
 
 
 def task(
@@ -43,27 +36,21 @@ def task(
     meta: Dict[str, Any] | None = None,
 ):
     def task_factory(func):
-        def create_task(*args, **kwargs):
-            task_instance = Task(
-                func=func,
-                input_type=input_type,
-                output_type=output_type,
-                meta=meta,
-            )
-            task_instance.set_args(*args, **kwargs)
-            return task_instance
-
-        return create_task
+        return Task(
+            func=func,
+            input_type=input_type,
+            output_type=output_type,
+            meta=meta,
+        )
 
     return task_factory
 
 
 if __name__ == "__main__":
 
-    @task()
+    @task(meta={"name": "adding_task"})
     def adder(a: int, b: int) -> None:
         return a + b
 
-    my_task = adder(3, 5)
-    print(my_task.execute())
-    print(my_task)
+    print(adder)
+    print(adder.execute(3, 5))
