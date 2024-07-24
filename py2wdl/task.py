d5fb2b4bff613aef0b2bb5ea62815d80d9273c67
joonyoung1 <joonyoung1023@gmail.com>
2024-07-24 15:17:52 +0900

    Implement task connection using input and output values #1 #3 #5
    currently support for sequential and parallel execution

--- py2wdl/task.py
+++ py2wdl/task.py
@@ -2,7 +2,60 @@ from __future__ import annotations
 import inspect
 from textwrap import dedent
 from typing import Optional, Callable, Iterable, Type, Any
-from wdl_types import WDLType, Boolean, Int, String, File
+
+
+class WDLValue:
+    def __init__(
+        self, parent_task: Optional[Task] = None, output_idx: Optional[int] = None
+    ) -> None:
+        self.name: str = str(id(self))
+        self.parent_task: Optional[Task] = parent_task
+        self.child_task: Optional[Task] = None
+        self.output_idx: Optional[int] = output_idx
+        self.input_idx: Optional[int] = None
+
+    def set_input(self, child_task: Task, input_idx: int) -> None:
+        self.child_task = child_task
+        self.input_idx = input_idx
+
+
+class Boolean(WDLValue):
+    def __init__(
+        self,
+        value: Optional[bool] = None,
+        parent_task: Optional[Task] = None,
+        output_idx: Optional[int] = None,
+    ) -> None:
+        super().__init__(parent_task, output_idx)
+        self.value: Optional[bool] = value
+
+
+class Int(WDLValue):
+    def __init__(
+        self,
+        value: Optional[bool] = None,
+        parent_task: Optional[Task] = None,
+        output_idx: Optional[int] = None,
+    ) -> None:
+        super().__init__(parent_task, output_idx)
+        self.value: Optional[int] = value
+
+
+class String(WDLValue):
+    def __init__(
+        self,
+        value: Optional[bool] = None,
+        parent_task: Optional[Task] = None,
+        output_idx: Optional[int] = None,
+    ) -> None:
+        super().__init__(parent_task, output_idx)
+        self.value: Optional[str] = value
+
+
+class File(String): ...
+
+
+class Condition(String): ...
 
 
 class Task:
@@ -10,22 +63,31 @@ class Task:
         self,
         func: Callable[..., Any],
         name: str,
-        input_types: Optional[Iterable[Type[WDLType]]] = None,
-        output_types: Optional[Iterable[Type[WDLType]]] = None,
+        input_types: Optional[Iterable[Type[WDLValue]]] = None,
+        output_types: Optional[Iterable[Type[WDLValue]]] = None,
         meta: Optional[dict[str, Any]] = None,
     ) -> None:
         self.func: Callable[..., Any] = func
         self.name: str = name
         self.input_types = input_types
-        self.output_types = output_types
+        self.output_values = tuple(
+            t(parent_task=self, output_idx=i) for i, t in enumerate(output_types)
+        )
         self.meta: Optional[dict[str, Any]] = meta
 
-    def __call__(self, *args: Any) -> Any:
-        for i, (received, expected) in enumerate(zip(args, self.input_types)):
-            if not isinstance(received, expected):
+    def __call__(self, *args: WDLValue) -> Any:
+        for i, (arg, t) in enumerate(zip(args, self.input_types)):
+            if not isinstance(arg, t):
                 raise TypeError(
-                    f"Expected type {expected} on parameter {i}, but got {type(received)}"
+                    f"Expected type {t} on parameter {i}, but got {type(arg)}"
                 )
+            else:
+                arg.set_input(self, i)
+
+        if len(self.output_values) > 1:
+            return self.output_values
+        else:
+            return self.output_values[0]
 
     def execute(self, *args: Any, **kwargs: Any) -> Any:
         return self.func(*args, **kwargs)
@@ -44,8 +106,8 @@ class Task:
 
 def task(
     name: Optional[str] = None,
-    input_types: Optional[Iterable[Type[WDLType]]] = None,
-    output_types: Optional[Iterable[Type[WDLType]]] = None,
+    input_types: Optional[Iterable[Type[WDLValue]]] = None,
+    output_types: Optional[Iterable[Type[WDLValue]]] = None,
     meta: Optional[dict[str, Any]] = None,
 ) -> Callable[..., Any]:
     def task_factory(func: Callable[..., Any]) -> Task:
@@ -64,15 +126,17 @@ if __name__ == "__main__":
 
     @task(
         name="adding_task",
-        input_types=(Int, Int),
-        output_types=(Int),
+        input_types=(File, Int),
+        output_types=(Int,),
         meta={"description": "simple adding task"},
     )
-    def adder(a: int, b: int) -> int:
+    def adder(a, b):
         return a + b
 
     print(adder)
 
     adder_input_a = File()
     adder_input_b = Int()
-    adder(adder_input_a, adder_input_b)
+    adder_output = adder(adder_input_a, adder_input_b)
+
+    print(adder_output)
