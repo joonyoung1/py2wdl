6d1191d6a934e8aa1a3db018cc49f155aa0cd4fe
joonyoung1 <joonyoung1023@gmail.com>
2024-08-13 21:39:00 +0900

    Extract source code from given task #14

--- py2wdl/manager.py
+++ py2wdl/manager.py
@@ -1,12 +1,17 @@
-from .workflow import Workflow
-from .task import Task
+import inspect
+from textwrap import dedent
+
+from .workflow import Workflow, WorkflowComponent
+from .task import Task, Tasks
 
 
 class WorkflowManager:
     def __init__(self) -> None:
-        self.root = None
+        self.components: set[WorkflowComponent] = set()
 
     def add_workflow(self, workflow: Workflow) -> None:
+        self.components.update(workflow.operands)
+
         base = workflow.operands[0]
         for other, operator in zip(workflow.operands[1:], workflow.operators):
             outputs = base.get_outputs()
@@ -35,3 +40,27 @@ class WorkflowManager:
                 raise ValueError(f"Unsupported operator")
                 
             base = other
+    
+    def translate(self) -> None:
+        root_components = []
+        for task in self.iterate_over_task():
+            self.create_runnable_script(task)
+    
+    def iterate_over_task(self):
+        for component in self.components:
+            if isinstance(component, Task):
+                yield component
+            elif isinstance(component, Tasks):
+                yield from component
+    
+    def create_runnable_script(self, task: Task) -> None:
+        func_source = self.get_func_source(task)
+    
+    def get_func_source(self, task: Task) -> str:
+        func_source = inspect.getsourcelines(task.func)[0]
+
+        for i, line in enumerate(func_source):
+            if line.strip().startswith("def "):
+                return dedent(''.join(func_source[i:])).strip()
+        
+        raise ValueError("Function definition not found in source lines.")
