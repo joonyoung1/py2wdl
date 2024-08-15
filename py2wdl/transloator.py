18585958ed7a33b85e1a000ab9d69dc550213860
joonyoung1 <joonyoung1023@gmail.com>
2024-08-15 20:14:04 +0900

    Refactor Translator class to improve readability

--- py2wdl/transloator.py
+++ py2wdl/transloator.py
@@ -7,6 +7,16 @@ from .task import Task, Int, Float, Boolean
 
 class Translator:
     def create_runnable_script(self, task: Task) -> None:
+        func_source = self.parse_func_source(task)
+        file_source = self.parse_file_source(task)
+        import_block = self.generate_import_block(func_source, file_source)
+        main_block = self.generate_main_block()
+
+        script_content = import_block + func_source + main_block
+        with open(f"./{task.name}.py", "w") as file:
+            file.write(script_content)
+
+    def parse_func_source(self, task: Task) -> str:
         func_source = inspect.getsourcelines(task.func)[0]
         for i, line in enumerate(func_source):
             if line.strip().startswith("def "):
@@ -14,43 +24,23 @@ class Translator:
                 break
         else:
             raise ValueError("Function definition not found in source lines.")
+        return func_source
 
+    def parse_file_source(self, task: Task) -> str:
+        file_path = inspect.getfile(task.func)
+        with open(file_path, "r") as file:
+            file_source = file.read()
+        return file_source
+
+    def generate_import_block(self, func_source: str, file_source: str) -> list[str]:
         func_tree = ast.parse(func_source)
         func_names = {
             node.id for node in ast.walk(func_tree) if isinstance(node, ast.Name)
         }
 
-        file_path = inspect.getfile(task.func)
-        with open(file_path, "r") as file:
-            file_source = file.read()
         file_tree = ast.parse(file_source)
-
-        needed_imports = self.parse_needed_imports(file_tree, func_names)
-        if "import sys" not in needed_imports:
-            needed_imports.append("import sys")
-        import_block = "\n".join(needed_imports) + "\n\n\n" if needed_imports else ""
-
-        main_block = '\n\n\nif __name__ == "__main__":\n'
-        for i, input_type in enumerate(task.input_types, 1):
-            if input_type == Int:
-                main_block += f"\tsys.args[{i}] = int(sys.args[{i}])\n"
-            elif input_type == Float:
-                main_block += f"\tsys.args[{i}] = float(sys.args[{i}])\n"
-            elif input_type == Boolean:
-                main_block += (
-                    f'\tsys.args[{i}] = True if sys.args[{i}] == "true" else False\n'
-                )
-        main_block += f"\t{task.name}(*sys.args[1:])"
-
-        script_content = import_block + func_source + main_block
-        with open(f"./{task.name}.py", "w") as file:
-            file.write(script_content)
-
-    def parse_needed_imports(
-        self, file_tree: ast.Module, func_names: set[str]
-    ) -> list[str]:
-        
         needed_imports = []
+
         for node in file_tree.body:
             if isinstance(node, ast.Import):
                 for alias in node.names:
@@ -69,4 +59,21 @@ class Translator:
                     )
                     needed_imports.append(import_stmt)
 
-        return needed_imports
+        if "import sys" not in needed_imports:
+            needed_imports.append("import sys")
+        return "\n".join(needed_imports) + "\n\n\n" if needed_imports else ""
+
+    def generate_main_block(self, task: Task) -> str:
+        main_block = '\n\n\nif __name__ == "__main__":\n'
+
+        for i, input_type in enumerate(task.input_types, 1):
+            if input_type == Int:
+                main_block += f"\tsys.args[{i}] = int(sys.args[{i}])\n"
+            elif input_type == Float:
+                main_block += f"\tsys.args[{i}] = float(sys.args[{i}])\n"
+            elif input_type == Boolean:
+                main_block += (
+                    f'\tsys.args[{i}] = True if sys.args[{i}] == "true" else False\n'
+                )
+
+        return main_block + f"\t{task.name}(*sys.args[1:])"
